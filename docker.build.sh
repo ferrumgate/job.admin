735f2accaff7deef8366c34f920384917e13c258
ferrum <dev@ferrumgate.com>
2024-06-02 04:59:31 +0300

    master worker tests are ready

--- docker.build.sh
+++ docker.build.sh
@@ -1,27 +1,42 @@
 #!/bin/bash
 
 set -e
+first="$1"
+second="$2"
 npm run build
 rm -rf node_modules/rest.portal2
 mkdir -p node_modules/rest.portal2
 cp -R node_modules/rest.portal/* node_modules/rest.portal2
 version=$(cat package.json | grep version | cut -d: -f2 | tr -d , | tr -d \" | tr -d " ")
-docker build -t job.admin . $1
-docker tag job.admin job.admin:$version
+docker build -t job.admin .
+docker tag job.admin "job.admin:$version"
 echo "job.admin:$version builded"
-docker tag job.admin registry.ferrumgate.zero/ferrumgate/job.admin:$version
+docker tag job.admin "registry.ferrumgate.zero/ferrumgate/job.admin:$version"
 docker tag job.admin registry.ferrumgate.zero/ferrumgate/job.admin:latest
-docker tag job.admin ferrumgate/job.admin:$version
+docker tag job.admin "ferrumgate/job.admin:$version"
 
-while true; do
-    read -p "do you want push to local registry y/n " yn
-    case $yn in
-    [Yy]*)
-        docker push registry.ferrumgate.zero/ferrumgate/job.admin:$version
-        docker push registry.ferrumgate.zero/ferrumgate/job.admin:latest
-        break
-        ;;
-    [Nn]*) exit ;;
-    *) echo "please answer yes or no." ;;
-    esac
-done
+execute() {
+    docker push registry.ferrumgate.zero/ferrumgate/job.admin:"$version"
+    docker push registry.ferrumgate.zero/ferrumgate/job.admin:latest
+    if [ "$first" == "--push" ] || [ "$second" == "--push" ]; then
+        docker push ferrumgate/job.admin:"$version"
+    fi
+
+}
+
+if [ "$first" == "--force" ] || [ "$second" == "--force" ]; then
+    execute
+    exit
+else
+    while true; do
+        read -r -p "do you want push to local registry y/n " yn
+        case $yn in
+        [Yy]*)
+            execute
+            break
+            ;;
+        [Nn]*) exit ;;
+        *) echo "please answer yes or no." ;;
+        esac
+    done
+fi
